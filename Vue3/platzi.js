edf25b45773d586e8e610ca1963189b7fd81bc03
alejopanto <jose.pantoja@testu.co>
2022-12-19 23:11:07 -0500

    add Reflect

--- Vue3/platzi.js
+++ Vue3/platzi.js
@@ -5,11 +5,15 @@ class PlatziReactive{
         // Proxy
         this.$data = new Proxy(this.origen, {
             get(target, name) {
-                if (name in target) {
-                    return target[name]
+                if (Reflect.has(target, name)) {
+                    return Reflect.get(target, name)
                 }
                 console.warn('La propiedad', name, 'no existe');
                 return "";
+            },
+            set(target, name, value) {
+                console.log('Modificado');
+                Reflect.set(target, name, value)
             }
         })
     }
@@ -18,13 +22,24 @@ class PlatziReactive{
         document.querySelectorAll("*[p-text]").forEach(el =>  {
             this.pText(el, this.$data, el.getAttribute("p-text"));
         })
+
+        document.querySelectorAll("*[p-model]").forEach(el => {
+            const name = el.getAttribute("p-model");
+            this.pModel(el, this.$data, name);
+
+            el.addEventListener("input", () => {
+                Reflect.set(this.$data, name, el.value)
+            });
+        });
     }
 
     pText(el, target, name) {
-        el.innerText = target[name];
+        el.innerText = Reflect.get(target, name);
     }
 
-    pModel() {}
+    pModel(el, target, name) {
+        el.value = Reflect.get(target, name)
+    }
 }
 
 var Platzi = {
