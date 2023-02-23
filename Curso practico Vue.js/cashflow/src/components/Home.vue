fc6906d69b218c4881932f520eb8c915eb839d20
alejopanto <jose.pantoja@testu.co>
2023-02-22 20:53:16 -0500

    Header and content

--- Curso practico Vue.js/cashflow/src/components/Home.vue
+++ Curso practico Vue.js/cashflow/src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <h1>Hola mdfk</h1>
-  </div>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+  </Layout>
 </template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Layout,
+    Header,
+  },
+};
+</script>
